4b483730741a0a362cee8ef939f5577842433cc1
Juan <juanjo_23@live.com>
2016-12-31 03:15:28 -0600

    More images

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function get_random_nums() {
-	var nums = [1,2,3,4,5,6,7,8,9],
+	var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
 	ranNums = [],
 	i = nums.length,
-	j = 0;
+	j = 0,
+	cant = 9;
 
-	while (i--) {
+	while (cant--) {
+	    i--;
 	    j = Math.floor(Math.random() * (i+1));
 	    ranNums.push(nums[j]);
 	    nums.splice(j,1);
@@ -13,51 +15,34 @@ function get_random_nums() {
 }
 
 function show_emoji(num) {
-	document.getElementById('emojis').style.display = 'none';				
-	document.getElementById('result').style.display = 'block';	
-	document.getElementById('img-result').src = "images/" + num + ".png";			
+  $('#emojis').hide();
+  $('#result').show();
+  $('#img-result').attr('src', "images/" + num + ".png"); 
+  $("html, body").animate({ scrollTop: 0 }, "fast");	
 }
 
-function show_second() {
-	$('#second_link').trigger('click');
-}
-/*
-$(document).on("pagecreate",function(event){
-	$( "#firstPopup" ).popup( "show" );
-});
-*/
-
-
-$(document).bind("pageinit", function(){
-	setTimeout(function(){
-		$('#first_link').trigger('click');
-	}, 500);
-    
-    $('#pop1').on('popupafterclose', function(){
-    	setTimeout(function(){
-    		$('#second_link').trigger('click');    
-    	}, 500);
-    });    
-
-	$('#pop2').on('popupafterclose', function(){
-    	setTimeout(function(){
-    		$('#third_link').trigger('click');    
-    	}, 500);
-    });
-});
-
-/*
-$("#closeFirst" ).click(function() {
-  alert( "Handler for .click() called." );
-});
-*/
-
-/*
 
 $( document ).ready(function() {
-	alert("adios Javier");
-  // Handler for .ready() called.
-
+  swal({
+    title: "Piensa un número de 2 dígitos",
+    imageUrl: "images/businessman.png",
+    confirmButtonText: "Listo",
+    closeOnConfirm: false
+  },
+	function(){
+    swal({
+      title: "Al número que pensaste, réstale la suma de sus digitos",
+      imageUrl: "images/calculating.png",
+      confirmButtonText: "Listo",
+      closeOnConfirm: false
+    },
+    function(){
+      swal({
+        title: "Ubica tu emoji y toca cualquier parte de la pantalla",
+        imageUrl: "images/decree.png",
+        confirmButtonText: "Listo",
+      });
+    });      
+	});  
 });
 
-*/
